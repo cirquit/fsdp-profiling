624497fdeccd2631a7dd607b47d5156a60b21958
Alexander Isenko <alex.isenko@protonmail.com>
2023-09-16 05:29:14 +0200

    maybe cai working?

--- cai/run_benchmark.sh
+++ cai/run_benchmark.sh
@@ -4,7 +4,7 @@ export TRAIN_STEP=20
 
 for MODEL_TYPE in "gpt2_medium"; do
   for DISTPLAN in "CAI_Gemini"; do
-    for BATCH_SIZE in 32; do
+    for BATCH_SIZE in 2; do #32; do
       for GPUNUM in 2; do
         MODEL_TYPE=${MODEL_TYPE} DISTPLAN=${DISTPLAN} BATCH_SIZE=${BATCH_SIZE} GPUNUM=${GPUNUM} \
         bash ./run_gemini.sh
